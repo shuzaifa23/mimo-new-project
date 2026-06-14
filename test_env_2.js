a8e22198a521e0e25e6e9bfa030a975924eda038
shuzaifa23 <shuzaifasamee@gmail.com>
2026-06-14 13:06:27 +0530

    Always show spin buttons for number inputs

--- test_env_2.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const dotenv = require('dotenv');
-
-const envConfig = dotenv.parse(fs.readFileSync('.env.local'));
-console.log('URL:', JSON.stringify(envConfig.NEXT_PUBLIC_SUPABASE_URL));
-console.log('KEY:', JSON.stringify(envConfig.NEXT_PUBLIC_SUPABASE_ANON_KEY));
